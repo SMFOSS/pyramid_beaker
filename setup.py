f41b025112a993cb2a1eff1a62c241052436d15f
Whit Morriss <whit@surveymonkey.com>
2011-02-17 16:41:24 -0800

    our pypi mirror is not tolerant of case insensitivity

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires = ['pyramid', 'beaker']
+requires = ['pyramid', 'Beaker']
 
 setup(name='pyramid_beaker',
       version='0.0',
